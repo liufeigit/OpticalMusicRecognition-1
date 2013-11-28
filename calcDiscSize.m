e9df0e5e7f4ff8765f82526451a89eb5faf497f7
Jonas Petersson <jonasbjornpetersson@gmail.com>
2013-11-28 15:28:08 +0100

    Added files

--- calcDiscSize.m
+++ calcDiscSize.m
@@ -1,4 +1,4 @@
-function [discSize] = calcDiscSize(linepos, lineheight)
+function [discSize] = calcDiscSize(lineDist)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % calcDiscSize calculates the disc size used for the structure element,
 % using the average distance between two lines
@@ -10,20 +10,6 @@ function [discSize] = calcDiscSize(linepos, lineheight)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dist = zeros(1, 15);
-sumDist = 0;
-nrOfDist = 0;
-for i = 1:size(linepos,2)
-    if mod(i, 5) == 0
-    else
-        dist(i) = abs(linepos(i) - linepos(i+1));
-    end
+discSize = round(lineDist/2);
 
-    if dist(i) ~= 0
-        dist(i) = (dist(i)-lineheight)/2;
-        sumDist = sumDist + dist(i);
-        nrOfDist = nrOfDist + 1
-    end
-end
-discSize = round(sumDist/nrOfDist);
 end
---
+++ calcLineDist.m
@@ -0,0 +1,27 @@
+function [lineDist] = calcLineDist(linepos, lineheight)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% calcLineDist calculates the average distance between two lines
+%
+% linepos: The lines' positions
+% lineheight: The average height of a line
+%
+% lineDist: the average distance between two lines
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+dist = zeros(1, size(linepos,2));
+sumDist = 0;
+nrOfDist = 0;
+for i = 1:size(linepos,2)
+    if mod(i, 5) ~= 0
+        dist(i) = abs(linepos(i) - linepos(i+1));
+    end
+
+    if dist(i) ~= 0
+        dist(i) = (dist(i)-lineheight);
+        sumDist = sumDist + dist(i);
+        nrOfDist = nrOfDist + 1;
+    end
+end
+lineDist = sumDist/nrOfDist;
+end
