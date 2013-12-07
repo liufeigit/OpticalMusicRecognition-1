c87dcd3df36490661ad2da3110f413f208dd3691
ekke84 <eriju796@student.liu.se>
2013-12-07 16:03:31 +0100

    Fix

--- readFindNotes.m
+++ readFindNotes.m
@@ -1,24 +1,27 @@
 function[outchar] = readFindNotes(centroid,lines)
 
 
-OutChar = {'a3','g3','f3','e3','d3','c3','b2','a2','g2','f2','e2','d2','c2'};
+OutChar = {'e4','d4','c4','b3','a3','g3','f3','e3','d3','c3','b2','a2','g2','f2','e2','d2','c2'};
 
-LookUpTable = zeros(1,13);
+LookUpTable = zeros(1,13+4);
 lineDist = 9.5;%calcLineDist(lines,0);
-
-LookUpTable(1)  = lines(1)-lineDist;
-LookUpTable(2)  = lines(1)-lineDist/2;
-LookUpTable(3)  = lines(1) ;
-LookUpTable(4)  = lines(1) + (lines(2)-lines(1))/2;
-LookUpTable(5)  = lines(2) ;
-LookUpTable(6)  = lines(2) + (lines(3)-lines(2))/2;
-LookUpTable(7)  = lines(3) ;
-LookUpTable(8)  = lines(3) + (lines(4)-lines(3))/2;
-LookUpTable(9)  = lines(4);
-LookUpTable(10) = lines(4) + (lines(5)-lines(4))/2;
-LookUpTable(11) = lines(5);
-LookUpTable(12) = lines(5)+lineDist/2;
-LookUpTable(13) = lines(5)+lineDist;
+LookUpTable(1)  = lines(1)-lineDist*3;
+LookUpTable(2)  = lines(1)-lineDist*(5/2);
+LookUpTable(3)  = lines(1)-lineDist*2;
+LookUpTable(4)  = lines(1)-lineDist*(3/2);
+LookUpTable(5)  = lines(1)-lineDist;
+LookUpTable(6)  = lines(1)-lineDist/2;
+LookUpTable(7)  = lines(1) ;
+LookUpTable(8)  = lines(1) + (lines(2)-lines(1))/2;
+LookUpTable(9)  = lines(2) ;
+LookUpTable(10)  = lines(2) + (lines(3)-lines(2))/2;
+LookUpTable(11)  = lines(3) ;
+LookUpTable(12)  = lines(3) + (lines(4)-lines(3))/2;
+LookUpTable(13)  = lines(4);
+LookUpTable(14) = lines(4) + (lines(5)-lines(4))/2;
+LookUpTable(15) = lines(5);
+LookUpTable(16) = lines(5)+lineDist/2;
+LookUpTable(17) = lines(5)+lineDist;
 LookUpTable = round(LookUpTable);
 
 distance =  abs(LookUpTable-ceil(centroid(1,2)));
--- tnm034.m
+++ tnm034.m
@@ -15,6 +15,7 @@ function [ strout ] = tnm034(image)
 
 
 %correct the image's rotation
+size(image)
 imagerot = houghrotate(image);
 imagerot = imagerot(:,10:end,:);
 %convert image to grayscale
@@ -25,64 +26,72 @@ gray2 = rgb2gray(imagerot);
 
 %get binary image 
 binImg = adaptivethreshBW(gray2);
-
-
-
-%remove the stafflines
-binImg = removestafflines(binImg, linepos, 3);
-
-
-% How many row 
-number_of_rows = size(linepos,2)/5;
-imshow(binImg)
-%linepos =linepos + 10;
-number_of_rows
-long_distance = zeros(1,number_of_rows);
-index = 1;
-for i= 1:1:size(linepos,2)
-    if(mod(i,5)==0)
-        if(i == size(linepos,2))
-            break;
-        else
-            long_distance(index) = linepos(i+1)-linepos(i);
-            index = index + 1;
-        end
-    end
-end
-long_distance
-long_distance_half = long_distance./2;
-long_distance_half(3) = long_distance_half(2); 
-
-split_image = cell(1,number_of_rows);
-% Split the image 
-for i = 1:1:number_of_rows
-    split_image{i} = binImg(linepos(5*(i-1)+1)-long_distance_half(i):linepos(5*(i-1)+5)+long_distance_half(i),:,:);
-end
-    linepostemp = zeros(3,5);
-    A  = linepos(1) -long_distance_half(1);
-    A1 = linepos(6) -long_distance_half(2); 
-    A2 = linepos(11) -long_distance_half(3);
-    linepostemp(1,:) = linepos(1:5)-A;
-    linepostemp(2,:) = linepos(6:10)-A1;
-    linepostemp(3,:) = linepos(11:15)-A2;
-
-
-
-
-%% Find the blobs for every note !
-%
-
-%figure;
-%imshow(open2);
-
-
-a = '';
-%imshow(split_image{2});
-for i = 3:3
-    a = [a,readsegment(split_image{i},linepostemp(i,:),10)];
-    a = [a,'n'];
-end
-a
+% 
+% 
+% 
+% %remove the stafflines
+% binImg = removestafflines(binImg, linepos, 3);
+% 
+% 
+% % How many row 
+% number_of_rows = size(linepos,2)/5;
+% 
+% long_distance_half = zeros(1,number_of_rows);
+% index = 1;
+% for i= 1:1:size(linepos,2)
+%     if(mod(i,5)==0)
+%         if(i == size(linepos,2))
+%             break;
+%         else
+%             long_distance_half(index) = (linepos(i+1)-linepos(i))/2;
+%             index = index + 1;
+%         end
+%     end
+% end
+% 
+% long_distance_half(end) = long_distance_half(end-1); 
+% split_image = cell(1,number_of_rows);
+% % Split the image 
+% for i = 1:1:number_of_rows
+%     split_image{i} = binImg(linepos(5*(i-1)+1)-long_distance_half(i):linepos(5*(i-1)+5)+long_distance_half(i),:,:);
+% end
+% 
+% linepostemp = zeros(number_of_rows,5);
+% A = zeros(1,number_of_rows);
+% j = 0;
+% for i = 1:number_of_rows
+%     A(i) = linepos((5*j+1)) -long_distance_half(i);
+%     linepostemp(i,:) = linepos(5*j+1:5*j+5)-A(i);
+%     j=j+1; 
+% end
+% 
+% 
+% %     A  = linepos(1) -long_distance_half(1);
+% %     A1 = linepos(6) -long_distance_half(2); 
+% %     A2 = linepos(11) -long_distance_half(3);
+% %     A3 = linepos(16) -long_distance_half(4);
+% %     linepostemp(1,:) = linepos(1:5)-A;
+% %     linepostemp(2,:) = linepos(6:10)-A1;
+% %     linepostemp(3,:) = linepos(11:15)-A2;
+% %     linepostemp(4,:) = linepos(16:20)-A3;
+% % 
+% % 
+% 
+% 
+% %% Find the blobs for every note !
+% %
+% 
+% %figure;
+% %imshow(open2);
+% 
+% 
+% a = '';
+% %imshow(split_image{2});
+% for i = 1:number_of_rows
+%     a = [a,readsegment(split_image{i},linepostemp(i,:),10)];
+%     a = [a,'n'];
+% end
+% a
 
 
 
